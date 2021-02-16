e59d4dd3d237c285856930ad5a65bb48f53ac97f
Rustique-Uwimpaye <rustique644@gmail.com>
2021-02-16 19:46:22 +0300

    contact us form

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
-        
-        if(this.scrollY > 500){
+
+
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        
+
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
-        
+    $('.navbar .menu li a').click(function () {
+
         $('html').css("scrollBehavior", "smooth");
     });
 
-    
-    $('.menu-btn').click(function(){
+
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-   
+
     var typed = new Typed(".typing", {
         strings: ["Software Developer", "UI Designer"],
         typeSpeed: 100,
@@ -48,22 +48,22 @@ $(document).ready(function(){
         loop: true
     });
 
-    
+
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
@@ -71,3 +71,49 @@ $(document).ready(function(){
     });
 });
 
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        status.classList.add("success");
+        status.innerHTML = "Thank You!";
+    }
+
+    function error() {
+        status.classList.add("error");
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
